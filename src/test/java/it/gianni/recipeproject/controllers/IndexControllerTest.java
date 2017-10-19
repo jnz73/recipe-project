089d4e23572915a17e6193d7e490f1963073e0c1
Gianni Fabriziani <gianni.fabriziani@gmail.com>
2017-10-19 15:41:04 +0200

    add more test on controller

--- src/test/java/it/gianni/recipeproject/controllers/IndexControllerTest.java
+++ src/test/java/it/gianni/recipeproject/controllers/IndexControllerTest.java
@@ -1,23 +1,20 @@
 package it.gianni.recipeproject.controllers;
 
 import it.gianni.recipeproject.domain.Recipe;
-import it.gianni.recipeproject.repositories.RecipeRepository;
 import it.gianni.recipeproject.services.RecipeService;
-import it.gianni.recipeproject.services.RecipeServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class IndexControllerTest {
 
@@ -38,9 +35,29 @@ public class IndexControllerTest {
     @Test
     public void getIndexPage() throws Exception {
 
-        assertEquals("index", indexController.getIndexPage(model));
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
+        String viewName = indexController.getIndexPage(model);
+
+
+        //then
+        assertEquals("index", viewName);
+
+
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 
 }
